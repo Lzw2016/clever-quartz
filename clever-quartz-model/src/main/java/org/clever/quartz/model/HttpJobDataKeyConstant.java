6a521e22550f2e7d688c75ee1f7030108db70a65
Lzw2016 <lizhiwei1993>
2018-10-25 21:57:12 +0800

    设计重构

--- clever-quartz-model/src/main/java/org/clever/quartz/model/HttpJobDataKeyConstant.java
+++ clever-quartz-model/src/main/java/org/clever/quartz/model/HttpJobDataKeyConstant.java
@@ -7,7 +7,7 @@ package org.clever.quartz.model;
 public class HttpJobDataKeyConstant {
 
     /**
-     * HTTP任务配置Key
+     * Http任务配置Key
      */
     public static final String HttpJobConfig = "HttpJobConfig";
 
@@ -15,4 +15,15 @@ public class HttpJobDataKeyConstant {
      * Http任务通知配置key
      */
     public static final String HttpJobNotice = "HttpJobNotice";
+
+    /**
+     * Http扩展任务数据(JobData) -- 使用 http head 传递 json 字符串 Map<String, String>
+     */
+    public static final String HttpJobExtendData = "HttpJobExtendData";
+
+    /**
+     * 发送认证通知的消息数据 -- 使用 http head 传递 json 字符串 Map<String, String> <br />
+     * 任务响应 http head 中存在此值就发送通知,优先级最高(相对HttpJobNotice配置)
+     */
+    public static final String NoticeMessageData = "NoticeMessageData";
 }
